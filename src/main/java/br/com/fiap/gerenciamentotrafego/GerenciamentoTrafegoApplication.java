55278eb0219676c261e7b8a4c00f7461b50270e6
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-08-09 18:32:28 -0300

    feat: inserindo dados no MondoDB Atlas ao inicar a aplicacao

--- src/main/java/br/com/fiap/gerenciamentotrafego/GerenciamentoTrafegoApplication.java
+++ src/main/java/br/com/fiap/gerenciamentotrafego/GerenciamentoTrafegoApplication.java
@@ -1,20 +1,48 @@
 package br.com.fiap.gerenciamentotrafego;
 
+import br.com.fiap.gerenciamentotrafego.dto.AcidenteCadastroDTO;
+import br.com.fiap.gerenciamentotrafego.dto.FeridoCadastroDTO;
+import br.com.fiap.gerenciamentotrafego.dto.RuaCadastroDTO;
+import br.com.fiap.gerenciamentotrafego.dto.VeiculoCadastroDTO;
+import br.com.fiap.gerenciamentotrafego.service.AcidenteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class GerenciamentoTrafegoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    @Autowired
+    private AcidenteService acidenteService;
 
-		SpringApplication.run(GerenciamentoTrafegoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoTrafegoApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
+        AcidenteCadastroDTO acidente = new AcidenteCadastroDTO(
+            LocalDateTime.of(2024, 8, 4, 13, 0),
+            "grave",
+            new RuaCadastroDTO(null,"Rua das palmeiras", 123, "12345678", "Sao Paulo", "SP"),
+            List.of(
+                new VeiculoCadastroDTO(null, null,"fez-4111", "March", 2018, null),
+                new VeiculoCadastroDTO(null, null,"tes-4789", "Versa", 2018, "Branco"),
+                new VeiculoCadastroDTO(null, null,"htu-5872", "Onix", 2018, "Preto")
+            ),
+            List.of(
+                new FeridoCadastroDTO(null, "Alfredo", null, "Costela fraturada", null, null),
+                new FeridoCadastroDTO(null, "Marcelo", "12345678910", null, null, "htu-5872"),
+                new FeridoCadastroDTO(null, null, "10987654321", null, null, null)
+            )
+        );
 
-	}
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.newInstance();
+        acidenteService.cadastrarNovoAcidente(acidente, uriBuilder);
+    }
 }
