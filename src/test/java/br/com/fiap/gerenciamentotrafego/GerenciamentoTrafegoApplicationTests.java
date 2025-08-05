a3f97cf347034c1d0f543e5d33bb996cbab52278
vinicius-leonel <viniciuslps.cms@gmail.com>
2025-08-05 15:47:15 -0300

    refactor: Refatorando e atualizando CI CD

--- src/test/java/br/com/fiap/gerenciamentotrafego/GerenciamentoTrafegoApplicationTests.java
+++ src/test/java/br/com/fiap/gerenciamentotrafego/GerenciamentoTrafegoApplicationTests.java
@@ -1,28 +1,7 @@
 package br.com.fiap.gerenciamentotrafego;
 
-import br.com.fiap.gerenciamentotrafego.controller.AutenticacaoControllerTest;
-import br.com.fiap.gerenciamentotrafego.controller.UsuarioControllerTest;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.util.Arrays;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
 class GerenciamentoTrafegoApplicationTests {
 
-    @Autowired
-    private org.springframework.core.env.Environment environment;
-
-    @Test
-    void testaProfileAtivo() {
-        System.out.println("Profile ativo: " + System.getProperty("spring.profiles.active"));
-
-        System.out.println("Profiles ativos: " + Arrays.toString(environment.getActiveProfiles()));
-    }
+    // Testes em https://github.com/viniciusleonel/testes-bdd-ger_de_traf
 
 }
